57d95ede8a982ec9f7563411d55d25f1e543c0ca
kylieeeeee <kelly97491234@gmail.com>
2022-06-28 08:56:00 -0400

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -22,6 +22,7 @@ st.title('🗃️LIS File Translation Tool🧰⚙️')
 ## Functions
 
 # Function for saving the user-defined dictionary
+@st.cache
 def create_panelDef(panel_file):
     """
         Translate the input file to a dictionary.
@@ -42,14 +43,19 @@ def create_panelDef(panel_file):
         panelDict[test_name] = {'Include': include, 'Material': material, 'Result_Test': roche_names}
     return panelDict
 
+
 # Function to save all dataframes to one single excel
 def dfs_to_excel(df_list, sheet_list): 
     output = BytesIO()
     writer = pd.ExcelWriter(output,engine='xlsxwriter')   
     for dataframe, sheet in zip(df_list, sheet_list):
         dataframe.to_excel(writer, sheet_name=sheet, startrow=0 , startcol=0, index=False)   
-    writer.save()
+        for column in dataframe:
+            column_length = max(dataframe[column].astype(str).map(len).max(), len(column))
+            col_idx = dataframe.columns.get_loc(column)
+            writer.sheets[sheet].set_column(col_idx, col_idx, column_length)
 
+    writer.save()
     processed_data = output.getvalue()
     return processed_data
 
@@ -59,6 +65,19 @@ path_medicare = 'data/Medicare Panels.csv'
 path_roche_tests = 'data/tests performed by instruments.csv'
 
 #=======================================================================================================#
+# """
+#     All the variables starting with 'st.session_state' are used for saving
+#     the corresponding variables to avoid renew.
+#     Can view the session_state as a dictionary.
+#     Key is the name to assess the variable data, value is the actual data/value store in that variable
+#     For example:
+#         count = 10, we dont want the program to reset this number, so we use session_state to store the value
+#         st.session_state.count = count
+#         First 'count' is the key value in the session_state dictionary.
+#         Second 'count' is the value we want to store in the dictionary.
+#         We can also update the session_state by st.session_state.count = 20
+# """
+
 
 ## Section 1: Upload the excel file that need translation
 st.header("1️⃣Select the file which needs translation:")
@@ -71,35 +90,42 @@ if 'list_of_LIS' not in st.session_state:
     st.session_state.list_of_LIS = list_of_LIS
 
 if uploaded_file is not None:
-    LIS_file = pd.ExcelFile(uploaded_file)
-    all_sheets = ['(Not Selected Yet)'] + LIS_file.sheet_names
+    try:
+        LIS_file = pd.ExcelFile(uploaded_file)
+        all_sheets = ['(Not Selected Yet)'] + LIS_file.sheet_names
+        
+        ## User select the sheet name that needs translation
+        selected_sheet = st.selectbox('Select the sheet name:', all_sheets, key='raw_data_selection')
 
-    ## User select the sheet name that needs translation
-    selected_sheet = st.selectbox('Select the sheet name:', all_sheets, key='raw_data_selection')
+        ## to read just one sheet to dataframe and display the sheet:
+        if selected_sheet != '(Not Selected Yet)':
+            LIS_sheet = pd.read_excel(LIS_file, sheet_name = selected_sheet)
+            st.session_state.raw_data = LIS_sheet
+            with st.expander("Click here to check the file you upoaded"):
+                st.write("Number of observations: " + str(len(LIS_sheet)))
+                st.write("Here are the first 10 rows of data")
+                st.write(LIS_sheet.head(10))
+            ID_column = st.selectbox("Select the column for patient ID", LIS_sheet.columns)
+            st.session_state.ID_column = ID_column
+            test_name_column = st.selectbox('Select the columns for LIS test names', LIS_sheet.columns)
+            st.session_state.test_name_column = test_name_column
 
-    ## to read just one sheet to dataframe and display the sheet:
-    if selected_sheet != '(Not Selected Yet)':
-        LIS_sheet = pd.read_excel(LIS_file, sheet_name = selected_sheet)
-        st.session_state.raw_data = LIS_sheet
-        with st.expander("Click here to check the file you upoaded"):
-            st.write("Number of observations: " + str(len(LIS_sheet)))
-            st.write("Here are the first 10 rows of data")
-            st.write(LIS_sheet.head(10))
-        ID_column = st.selectbox("Select the column for patient ID", LIS_sheet.columns)
-        test_name_column = st.selectbox('Select the columns for LIS test names', LIS_sheet.columns)
+            if st.button('📤 Upload Raw Data'):
+                # create LIS objects for each row
+                try:
+                    for i in range(len(LIS_sheet)):
+                        patient_id = LIS_sheet[ID_column][i]
+                        test_name = str(LIS_sheet[test_name_column][i])
+                        tmp = LIS_Data(patient_id, test_name)
+                        list_of_LIS.append(tmp)
+                        st.session_state.list_of_LIS = list_of_LIS
 
-        if st.button('📤 Upload 📤'):
-            # create LIS objects for each row
-            try:
-                for i in range(len(LIS_sheet)):
-                    patient_id = LIS_sheet[ID_column][i]
-                    test_name = str(LIS_sheet[test_name_column][i])
-                    tmp = LIS_Data(patient_id, test_name)
-                    list_of_LIS.append(tmp)
-                    st.session_state.list_of_LIS = list_of_LIS
+                except AttributeError:
+                    st.warning("🚨 There are invalid test names")
 
-            except AttributeError:
-                st.warning("🚨 There are invalid test names 🚨")
+    except ValueError:
+        st.error("🚨The file you upload is not an Excel file (.xlsx)")
+    
 
 #=======================================================================================================#
 
@@ -119,38 +145,42 @@ if dict_source == 'Upload my own dictionary':
     st.info("The column names of the first 4 columns should be exactly the same as **Test Name**, **Include**, **Material**, and **Result_Test**.")
     uploaded_dict = st.file_uploader("Select the excel file. Please make sure the file follows the format.")
     if uploaded_dict is not None:
-        own_dict = pd.ExcelFile(uploaded_dict)
-        all_dict_sheets = ['(Not Selected Yet)'] + own_dict.sheet_names
+        try:
+            own_dict = pd.ExcelFile(uploaded_dict)
+            all_dict_sheets = ['(Not Selected Yet)'] + own_dict.sheet_names
 
-        ## User select the sheet name that needs translation
-        selected_dict_sheet = st.selectbox('Select the sheet name:', all_dict_sheets, key='dictionary_selection')
+            ## User select the sheet name that needs translation
+            selected_dict_sheet = st.selectbox('Select the sheet name:', all_dict_sheets, key='dictionary_selection')
 
-        ## to read just one sheet to dataframe and display the sheet:
-        if selected_dict_sheet != '(Not Selected Yet)':
-            try:
-                own_dict_sheet = pd.read_excel(own_dict, sheet_name = selected_dict_sheet)
-                own_dict_sheet['Result_Test'].fillna('NA', inplace=True)
-                st.session_state.own_dict_sheet = own_dict_sheet
-                with st.expander("Click here to check the dictionary you uploaded"):
-                    st.write("Number of observations: " + str(len(own_dict_sheet)))
-                    st.write("Here are the first 10 rows of data")
-                    st.write(own_dict_sheet.head(10))
+            ## to read just one sheet to dataframe and display the sheet:
+            if selected_dict_sheet != '(Not Selected Yet)':
+                try:
+                    own_dict_sheet = pd.read_excel(own_dict, sheet_name = selected_dict_sheet)
+                    own_dict_sheet['Result_Test'].fillna('NA', inplace=True)
+                    st.session_state.own_dict_sheet = own_dict_sheet
+                    with st.expander("Click here to check the dictionary you uploaded"):
+                        st.write("Number of observations: " + str(len(own_dict_sheet)))
+                        st.write("Here are the first 10 rows of data")
+                        st.write(own_dict_sheet.head(10))
 
-                # If the button is clicked, app will save the dictionary
-                if st.button('📤 Upload Dictionary 📤'):
-                    panelDict = create_panelDef(own_dict_sheet)
-                    st.session_state.panelDict = panelDict
+                    # If the button is clicked, app will save the dictionary
+                    if st.button('📤 Upload Dictionary'):
+                        panelDict = create_panelDef(own_dict_sheet)
+                        st.session_state.panelDict = panelDict
 
-            except KeyError:
-                st.warning('🚨 Your dictionary does not follow the naming conventions 🚨')
+                except KeyError:
+                    st.warning('🚨 Your dictionary does not follow the naming conventions')
+
+        except ValueError:
+            st.error("🚨The file you upload is not an Excel file (.xlsx)")
+
+
 
 # User select to use the base dictionary
 # base dictionary is the medicare panels
 if dict_source == 'Use the base dictionary':
-    #medicare_panel = pd.read_csv('Medicare Panels.csv', index_col=0)
-    #Use the below code to upload data if running online
     medicare_panel = pd.read_csv(path_medicare, index_col = 0
-    
+
     medicare_panel['Result_Test'].fillna('NA', inplace=True)
     st.session_state.medicare_panel = medicare_panel
     with st.expander("Click here to check the base dicitonary"):
@@ -161,9 +191,6 @@ if dict_source == 'Use the base dictionary':
 
 
 # Read in all roche test names by different instruments and save in rocheDef
-# roche_test_by_platform = pd.read_csv('tests performed by instruments.csv', index_col=0)
-
-#Use the below code to upload data if running online
 roche_test_by_platform = pd.read_csv(path_roche_tests, index_col=0)
 
 # create an empty dictionary for saving all roche test names
@@ -240,11 +267,14 @@ if st.button('🖱️ Click here to start matching ⏳'):
             st.session_state.list_of_LIS = matched_LIS
             st.session_state.new_panelDict = new_panelDict
 
-        st.success("✅Matching finished!🎉🎉")
+        st.success("🎉Matching finished! Generating result file...")
 
 
 # Process the output
-        # Process the objects into a dataframe.
+        # Raw Data
+        raw_data = st.session_state.raw_data
+
+        # Matched result save as a dataframe
         matched_LIS = st.session_state.list_of_LIS
         tmp = []
         for i in range(len(matched_LIS)):
@@ -253,6 +283,15 @@ if st.button('🖱️ Click here to start matching ⏳'):
 
         matched_df = pd.DataFrame(tmp)
         matched_df.columns = ['Patient ID', 'LIS Test Name', 'Assay Name', 'Source']
+
+
+        # Joining the raw data with matched results
+        merged_raw = pd.merge(raw_data, matched_df, 
+                            left_on = [st.session_state['ID_column'], st.session_state['test_name_column']], 
+                            right_on = ['Patient ID', 'LIS Test Name'])
+
+
+        merged_raw = merged_raw.explode('Assay Name')
         matched_df = matched_df.explode('Assay Name')
         matched_df = matched_df.astype(str)
         st.session_state.matched_df = matched_df
@@ -269,10 +308,10 @@ if st.button('🖱️ Click here to start matching ⏳'):
         #new_dict.columns = [v if 'Unnamed' not in col else col for col, v in new_dict.iloc[0].items()]
         new_dict = pd.concat([old_dict, new_dict], axis=0).reset_index().drop('index', axis = 1)
 
-        # raw data
-        raw_data = st.session_state.raw_data
 
-        df_xlsx = dfs_to_excel([new_dict, matched_df, raw_data],['Panel Definitions', 'Graph Data Worksheet', 'raw data'])
+        # output the excel file and let the user download
+        sheet_list = ['Panel Definitions', 'Graph Data Worksheet', 'raw data with matching results', 'raw data']
+        df_xlsx = dfs_to_excel([new_dict, matched_df, merged_raw, raw_data],  sheet_list)
         st.download_button(label='📥 Download Current Result 📥',
                                         data=df_xlsx ,
                                         file_name= 'df_test.xlsx')
