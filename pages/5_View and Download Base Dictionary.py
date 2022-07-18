e58d513ba1c750efb5878f1b6d6f82b918164473
kylieeeeee <kelly97491234@gmail.com>
2022-07-18 16:43:27 -0400

    Update 5_View and Download Base Dictionary.py

--- pages/5_View and Download Base Dictionary.py
+++ pages/5_View and Download Base Dictionary.py
@@ -13,14 +13,32 @@ st.set_page_config(page_title="LIS Translation Tool", page_icon='🗃️',
 st.title('🗃️LIS File Translation Tool🧰⚙️')
 st.header('View and Download Base Dictionary')
 
-
 base_dict = f.load_json('data/LIS DB.json')
 base_dict = pd.DataFrame.from_dict(base_dict, orient='index').reset_index(drop = False)
 base_dict.rename(columns={'index': 'LIS Test Name', 'AssayName': 'Assay Name'}, inplace = True)
 
-st.dataframe(base_dict, width=1400)
+with st.expander('Click here to see the interactivity of the table'):
+    st.markdown("""
+    ### Interactivity
+Dataframes displayed as interactive tables with st.dataframe have the following interactive features:
+- **Column sorting**: sort columns by clicking on their headers.
+- **Column resizing**: resize columns by dragging and dropping column header borders.
+- **Table (height, width) resizing**: resize tables by dragging and dropping the bottom right corner of tables.
+- **Search**: search through data by clicking a table, using hotkeys (⌘ Cmd + F or Ctrl + F) to bring up the search bar, and using the search bar to filter data.
+- **Copy to clipboard**: select one or multiple cells, copy them to clipboard, and paste them into your favorite spreadsheet software.
+""")
+
+st.dataframe(base_dict, width=1000)
 
 df_xlsx = f.dfs_to_excel([base_dict], ['Base Dictionary'])
 st.download_button(label='📥 Download Base Dictionary 📥',
                                 data=df_xlsx,
                                 file_name='Base Dictionary.xlsx')
+
+st.markdown("""
+### Please fill out this google form if you find out a LIS test should be included in the base dictionary
+*We will update the base dictionary every month.*
+- [Link to google form]
+- [View submitted results]
+
+""")
