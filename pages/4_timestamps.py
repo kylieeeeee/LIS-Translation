48cd41bb72a0b310157897b272a833f5dd24c7b4
kylieeeeee <kelly97491234@gmail.com>
2022-07-18 09:47:41 -0400

    Add files via upload

---
+++ pages/4_timestamps.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import functions as f
+from datetime import datetime
+
+st.set_page_config(page_title="LIS Translation Tool", page_icon='🗃️', 
+                layout="wide",
+                initial_sidebar_state="expanded",
+     menu_items={
+         'About': "# This is the LIS file translation tool."
+     })
+
+
+st.title('🗃️LIS File Translation Tool🧰⚙️')
+st.subheader('Fill in the missing timestamps and format the date and time')
+
+
+## Section 1: Upload the excel file that need translation
+st.header('Upload Raw Data')
+uploaded_file = st.file_uploader("Select the file which needs translation:")
+st.info('Please only upload excel file.')
+
+# list to save all LIS_Data objects
+list_of_LIS = []
+if 'list_of_LIS' not in st.session_state:
+    st.session_state.list_of_LIS = list_of_LIS
+
+if uploaded_file is not None:
+    try:
+        # get the file name of raw data
+        st.session_state.file_name = uploaded_file.name
+        LIS_file = pd.ExcelFile(uploaded_file)
+        all_sheets = ['(Not Selected Yet)'] + LIS_file.sheet_names
+        
+        ## User select the sheet name that needs translation
+        selected_sheet = st.selectbox('Select the sheet name:', all_sheets, key='raw_data_selection')
+
+        ## to read just one sheet to dataframe and display the sheet:
+        if selected_sheet != '(Not Selected Yet)':
+            raw_data = pd.read_excel(LIS_file, sheet_name = selected_sheet)
+            ID_column = st.selectbox("Select the column for patient ID", raw_data.columns)
+            st.session_state.ID_column = ID_column
+            if raw_data[ID_column].isna().sum() > 0:
+                st.warning('WARNING: There are missing patient ID in this data.  Rows without patient ID will be dropped durning translation.')
+            raw_data[ID_column] = raw_data[ID_column].astype(str)
+            st.session_state.raw_data = raw_data
+
+            with st.expander("Click here to check the file you upoaded"):
+                st.write("Number of observations: " + str(len(raw_data)))
+                st.write("Here are the first 10 rows of raw data")
+                st.write(raw_data.head(10))
+                st.caption("<NA> means there is no value in the cell")
+
+            st.markdown('---')
+            # select the timestamp columns
+            time_columns = st.multiselect("Select the column of timestamps that need formatting", raw_data.columns)
+            st.info('Please only select the timestamp columns which include BOTH date and time')
+            # input/choose delimiter
+            delimiter = st.radio('Select the delimiter that separate date and time in a timestamp column',
+                            ('Space', '@', '_'))
+            if delimiter == 'Space':
+                delimiter = ' '
+
+            # fill in missing dates
+            filled_data = raw_data.copy()
+            filled_data[time_columns] = filled_data.groupby(ID_column)[time_columns].ffill().bfill()
+            st.session_state.filled_data = filled_data
+
+            # separate the date and time
+            # delimiter???
+            # If data type of time columns is string
+            for col in time_columns:
+                try:
+                    # create the new column name
+                    date_col = col + '__Date'
+                    time_col = col + '__Time'
+                    # filled_data[col] = filled_data[col].astype(str)
+                    # filled_data[[date_col, time_col]] = filled_data[col].str.split(delimiter, expand = True)
+
+                    # check the data type of the timestamp column
+                    if filled_data[col].dtypes == 'object':
+                        filled_data[[date_col, time_col]] = filled_data[col].str.split(delimiter, expand = True)
+                        st.session_state.filled_data = filled_data
+                    elif filled_data[col].dtypes == 'datetime64[ns]':
+                        filled_data[date_col] = filled_data[col].apply(lambda x: x.strftime("%Y/%m/%d"))
+                        filled_data[time_col] = filled_data[col].apply(lambda x: x.strftime("%H:%M:%S"))
+                    else:
+                        st.write(filled_data[col].dtypes)
+                        st.warning('Unknown data type')
+
+                except ValueError:
+                    st.error('At least one of the columns you selected does not include BOTH date and time')
+
+
+            st.markdown('---')
+            st.write('Preview the new data')
+            st.info('Please scroll to the right to see the newly created columns')
+            st.write(filled_data)
+            st.caption("<NA> means there is no value in the cell")
+
+
+
+            # Formatting the new file name
+            today = datetime.today().strftime("%Y%m%d_%H%M")+'_'
+            new_file_name = 'Time Formatted_' + today + st.session_state.file_name 
+            # output the excel file and let the user download
+            sheet_list = ['Time Formatted Data', 'Raw Data']
+            df_xlsx = f.dfs_to_excel([filled_data, raw_data], sheet_list)
+            st.download_button(label='📥 Download Current Result 📥',
+                                            data=df_xlsx,
+                                            file_name= new_file_name)
+            
+    except ValueError:
+        st.error("🚨The file you upload is not an Excel file (.xlsx)")
+    
